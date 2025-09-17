8e4ba7c24b533531a62f4bbe1aaffff4ef3f5508
rajan yadav <rajanyadavsurapur@gmail.com>
2025-09-17 07:19:00 +0530

    second commit

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+operator=input("enter the operator;+,-,*,/,%")
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+def sum(a,b):
+    if (operator=="+"):
+        return a+b
+def sub(a,b):
+    if (operator=="-"):
+        return a-b
+def mul(a,b):
+    if (operator=="*"):
+        return a*b
+def div(a,b):
+    if (operator=="/"):
+        return a/b
+def mod(a,b):
+    if (operator=="%"):
+        return a%b
+print("the result is:",sum(a,b)or sub(a,b) or mul(a,b) or div(a,b) or mod(a,b))
+
